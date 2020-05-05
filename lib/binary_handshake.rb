9c7d7dc5948afcb8808d48ef3c82b66a4ca17038
Pat McClernan <patmccler@gmail.com>
2020-05-05 14:08:46 -0400

    implements methods

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -1,9 +1,31 @@
-# Binary Handshake Rules
+class SecretHandshake
+  attr_reader :commands
 
-# 1 = "wink"
-# 10 = "double blink"
-# 100 = "close your eyes"
-# 1000 = "jump"
-# 10000 = Reverse the order of the operations in the secret handshake.
+  def initialize input
+    @commands = process input
+  end
 
-# Write your code here.
+  def process(input)
+    chars = input.split("").reverse
+    return [] unless chars.all? { |c| %(1 0).include? c }
+
+    chars.each_with_object([]).with_index do |(char, arr), i|
+      next unless char == "1"
+
+      case i
+      when 0
+        arr << "wink"
+      when 1
+        arr << "double blink"
+      when 2
+        arr << "close your eyes"
+      when 3
+        arr << "jump"
+      when 4
+        arr.reverse!
+      end
+      # case
+    end
+    # each with index
+  end
+end
